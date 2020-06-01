0ef209690d0de5d12795b0ce5729e6c9ab5a9f58
Nirlipta13 <nirliptadas13@gmail.com>
2020-06-01 11:11:46 +0530

    Font Sizing

--- src/app/symptoms-and-precautions/symptoms-and-precautions.component.ts
+++ src/app/symptoms-and-precautions/symptoms-and-precautions.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'symptoms-and-precautions',
   templateUrl: './symptoms-and-precautions.component.html',
   styleUrls: ['./symptoms-and-precautions.component.css']
